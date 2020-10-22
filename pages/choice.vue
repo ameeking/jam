61f92435236698a26a2c2f5076d5aef18044ef1d
ameeking <aaronmeeking@gmail.com>
2020-10-22 22:37:05 +1100

    First pages

---
+++ pages/choice.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>ao ora</h1>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+<main>
+  <h1>Hello world!</h1>
+  <NuxtLink to="/">Home</NuxtLink>
+  <NuxtLink to="/choice">Choice</NuxtLink>
+</main>
+</template>
